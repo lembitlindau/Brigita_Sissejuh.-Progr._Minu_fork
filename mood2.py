3425c96ecd2343e6683002c07552f59b09635c90
Brigita Kasemets <brigitakasemets@MacBookAir.lan>
2024-09-26 19:45:43 +0300

    Initial commit

---
+++ mood2.py
@@ -0,0 +1,23 @@
+nimi = input('Sisesta firma nimi: ')
+toode = input('Sisesta toote nimetus: ')
+
+laoseis = {
+    'kleit': 5,
+    'särk': 1,
+    'püksid': 0
+}
+
+minimaalne_laoseis = 3
+
+def kontrolli_laoseisu(toode, kogus):
+    if kogus == 0:
+        print(f"{toode}: Laost otsas! Telli kohe juurde.")
+    elif kogus < minimaalne_laoseis:
+        print(f"{toode}: Lähiajal vaja juurde tellida. Laoseis: {kogus}")
+    else:
+        print(f"{toode}: Laoseis on piisav. Laoseis: {kogus}")
+
+for toode, kogus in laoseis.items():
+    kontrolli_laoseisu(toode, kogus)
+
+print(f'Tere, {nimi}. Teie laoseis tootele {toode} on {laoseis.get(toode, "Toode ei ole laoseisus")}.')
